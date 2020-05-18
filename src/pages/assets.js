b012bda8779a839fa6b81bda5c5adb051902a05b
litianao <exrobbie@gmail.com>
2020-05-18 21:45:25 +0800

    changed types

--- src/pages/assets.js
+++ src/pages/assets.js
@@ -13,6 +13,7 @@ import TransferForm from 'components/forms/assets/TransferForm';
 export default function P2p(props) {
     const { api } = useSubstrate();
     const [assetsList, setAssetsList] = useState(0);
+    const [originalAssetsList, setOriginalAssetsList] = useState(0);
     const [symbolsMapping, setSymbolsMapping] = useState({});
     const [transferModalVisible, setTransferModal] = useState(false)
     const [selectingItem, setSelectingItem] = useState(0);
@@ -32,15 +33,16 @@ export default function P2p(props) {
 
     useEffect(() => {
         if (accountPair) {
-            let assetArray = []
             // Get generic asset
             api.rpc.genericAsset.userAssets(accountPair.address).then(res => {
-                assetArray = JSON.parse(res)
-                // Get DFX asset
-                api.query.system.account(accountPair.address).then(result => {
-                    assetArray.push({ asset_id: '', symbol: 'DFX', balance: Number(result.data.free), isMain: true })
-                    setAssetsList(assetArray)
-                })
+                setAssetsList(JSON.parse(res))
+            })
+
+            // Get DFX asset
+            api.query.system.account(accountPair.address).then(result => {
+                setOriginalAssetsList([{
+                    asset_id: '', symbol: 'DFX', balance: Number(result.data.free), isMain: true
+                }])
             })
         }
     }, [api.rpc.pToP, api.rpc.genericAsset, api.query.system, symbolsMapping, accountPair, refreshKey]);
@@ -57,9 +59,6 @@ export default function P2p(props) {
                     )}
                     {record.symbol === 'DUSD' && (
                         <img src={UsdtIcon} alt="DUSD Icon" style={{ width: '30px', height: '30px', marginRight: '8px' }} />
-                    )}
-                     {record.symbol === 'DFX' && (
-                        <img src={DfxIcon} alt="DFX Icon" style={{ width: '30px', height: '30px', marginRight: '8px' }} />
                     )}
                     {record.symbol}
                 </div>
@@ -89,13 +88,49 @@ export default function P2p(props) {
         }
     ]
 
+    const originalColumns = [
+        {
+            title: 'Symbol',
+            dataIndex: 'symbol',
+            key: 'symbol',
+            render: (props, record) => (
+                <div>
+                    {record.symbol === 'DFX' && (
+                        <img src={DfxIcon} alt="DFX Icon" style={{ width: '30px', height: '30px', marginRight: '8px' }} />
+                    )}
+                    {record.symbol}
+                </div>
+            )
+        },
+        {
+            title: 'Balance',
+            dataIndex: 'balance',
+            key: 'balance',
+            render: (props, record) => (
+                <span>{record.balance / (10 ** 8)} {record.symbol}</span>
+            )
+        },
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            key: 'action',
+            render: (props, record) => (
+                <Button onClick={() => { setSelectingItem(record); setTransferModal(true) }}>Transfer</Button>
+            )
+        }
+    ]
+
     return (
         <div>
             <Card style={{ margin: '32px auto' }}>
                 <div className={'card-head'}>
-                    <div className={'card-title'}>Assets</div>
+                    <div className={'card-title'}>Generic Assets</div>
                 </div>
-                <Table columns={columns} rowKey={'asset_id'} dataSource={assetsList} pagination={false} />
+                <Table columns={columns} rowKey={'symbol'} dataSource={assetsList} pagination={false} style={{ marginBottom: '32px' }} />
+                <div className={'card-head'}>
+                    <div className={'card-title'}>Balance</div>
+                </div>
+                <Table columns={originalColumns} rowKey={'symbol'} dataSource={originalAssetsList} pagination={false} />
             </Card>
             {transferModalVisible && <Modal
                 title={'Transfer'}
